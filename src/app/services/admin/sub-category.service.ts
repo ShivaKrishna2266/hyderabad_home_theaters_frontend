62b661705bde97cb84474dc215776f7371727758
ShivaKrishna2266 <gadudulashiva@gmail.com>
2025-03-29 19:02:54 +0530

    Added SubCategory Component And Service & Product Componet And Service in Admin DashBord

---
+++ src/app/services/admin/sub-category.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environments';
+import { UserStorageService } from '../storege/user-storege.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubCategoryService {
+  private apiUrl = environment.apiUrl;
+ 
+   constructor(
+               private userStorageService:UserStorageService,
+               private http: HttpClient,
+   ) { }
+ 
+   getAllSubCategories(): Observable<any>{
+     const token = localStorage.getItem('token');
+     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+     return this.http.get<any>(this.apiUrl + "/admin/getAllSubCategories",{headers});
+   }
+  }
